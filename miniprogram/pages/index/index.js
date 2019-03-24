d2fa8a40ef70fe9ad612f465bc5a7f9f39b366da
wangyangyy <wangyangyy@ipanel.com>
2019-03-24 17:14:35 +0800

    登录界面

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,10 +7,18 @@ Page({
     userInfo: {},
     logged: false,
     takeSession: false,
-    requestResult: ''
+    requestResult: '',
+    identify:['我是学生','我是老师'],
+    index:0
   },
     dosomething(timeStamp){
         console.log(timeStamp)
+    },
+    bindPickerChange(e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            index: e.detail.value
+        })
     },
   onLoad: function() {
     if (!wx.cloud) {
@@ -48,76 +56,76 @@ Page({
     }
   },
 
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
-  },
+//   onGetOpenid: function() {
+//     // 调用云函数
+//     wx.cloud.callFunction({
+//       name: 'login',
+//       data: {},
+//       success: res => {
+//         console.log('[云函数] [login] user openid: ', res.result.openid)
+//         app.globalData.openid = res.result.openid
+//         wx.navigateTo({
+//           url: '../userConsole/userConsole',
+//         })
+//       },
+//       fail: err => {
+//         console.error('[云函数] [login] 调用失败', err)
+//         wx.navigateTo({
+//           url: '../deployFunctions/deployFunctions',
+//         })
+//       }
+//     })
+//   },
 
   // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
+//   doUpload: function () {
+//     // 选择图片
+//     wx.chooseImage({
+//       count: 1,
+//       sizeType: ['compressed'],
+//       sourceType: ['album', 'camera'],
+//       success: function (res) {
 
-        wx.showLoading({
-          title: '上传中',
-        })
+//         wx.showLoading({
+//           title: '上传中',
+//         })
 
-        const filePath = res.tempFilePaths[0]
+//         const filePath = res.tempFilePaths[0]
         
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
+//         // 上传图片
+//         const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+//         wx.cloud.uploadFile({
+//           cloudPath,
+//           filePath,
+//           success: res => {
+//             console.log('[上传文件] 成功：', res)
 
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
+//             app.globalData.fileID = res.fileID
+//             app.globalData.cloudPath = cloudPath
+//             app.globalData.imagePath = filePath
             
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
+//             wx.navigateTo({
+//               url: '../storageConsole/storageConsole'
+//             })
+//           },
+//           fail: e => {
+//             console.error('[上传文件] 失败：', e)
+//             wx.showToast({
+//               icon: 'none',
+//               title: '上传失败',
+//             })
+//           },
+//           complete: () => {
+//             wx.hideLoading()
+//           }
+//         })
 
-      },
-      fail: e => {
-        console.error(e)
-      }
-    })
-  },
+//       },
+//       fail: e => {
+//         console.error(e)
+//       }
+//     })
+//   },
 
 
 })
